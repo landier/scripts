1f54f0b7a5f01880d27657ae9dc706d96bc8ffe1
Nicolas Landier <fake@email.net>
2014-02-12 14:49:14 -0200

    Renamed to link_generator.py

--- link_generator.py
+++ link_generator.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import urllib, urllib2
+import getopt
 
 
 URL_SHORTENER_API = 'https://www.googleapis.com/urlshortener/v1/url'
@@ -39,16 +40,25 @@ def generate_url_for_channel(url, source, medium=None):
 
 
 def shorten_url(url):
-    postdata = {'longUrl':url}
-    headers = {'Content-Type':'application/json'}
-    req = urllib2.Request(
-        URL_SHORTENER_API,
-        json.dumps(postdata),
-        headers
-    )
-    ret = urllib2.urlopen(req).read()
-    #print ret
-    return json.loads(ret)['id']
+	postdata = {'longUrl':url}
+	headers = {'Content-Type':'application/json'}
+	req = urllib2.Request(
+		URL_SHORTENER_API,
+		json.dumps(postdata),
+		headers
+	)
+	ret = urllib2.urlopen(req).read()
+	#print ret
+	return json.loads(ret)['id']
+
+
+def help():
+	print "Help"
+	print "ling_generator.py --source <url> --medium=<medium> --campaign==<campaign_name>"
+	print "-h --help\t\t\tShow this help message."
+	print "-s --source\t\t\tDefine the source url the links will be redirecting to."
+	print "-m --medium\t\t\tDefine a specific medium to be dinstiguished in analytics."
+	print "-c --campaign\t\t\tDefine the campaign name to be displayed in analytics. By default, the campaign name is <url>."
 
 
 if __name__ == "__main__":
@@ -57,4 +67,16 @@ if __name__ == "__main__":
 	if len(sys.argv) > 2:
 		generate_urls(sys.argv[1], sys.argv[2])
 	else:
-		generate_urls(sys.argv[1])     
+		generate_urls(sys.argv[1])	 
+
+	try:								
+		opts, args = getopt.getopt(argv, "hs:m:c:", ["help", "source=", "medium=", "campaign="])
+	except getopt.GetoptError:		  
+		sys.exit(2)					 
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			help()					 
+			sys.exit()				  
+		else if opt in ("-s", "--source"):
+			
+			generate_urls(sys.argv[1])
